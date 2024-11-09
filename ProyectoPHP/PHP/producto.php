6a4b1a277fefc79922b30110a8cc87b6b68f3c65
alfredo <alfredo.puentes25@outlook.com>
2024-11-09 00:00:26 -0600

    identificar productos

--- ProyectoPHP/PHP/producto.php
+++ ProyectoPHP/PHP/producto.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:ProyectoPHP/PHP/producto.php
 <?php include 'includes/header.php';
 
 // Confirmar sesión
@@ -12,4 +13,27 @@ if(!isset($_SESSION['loggedin'])) {
 
 // include 'footerProd.html';
 
+=======
+<?php
+
+include 'headerProd.html';
+
+// Confirmar sesión
+session_start();
+
+if(!isset($_SESSION['loggedin'])) {
+    header('Location: productos.php');
+    exit();
+}
+
+if (isset($_GET['producto'])) {
+    $producto = $_GET['producto'];
+    echo "<h1 class=\"subtitulo\"> Esta usted viendo el producto: ". $producto."</h1>";
+} else {
+    echo "<h1 class=\"subtitulo\"> Esta usted viendo todos los productos </h1>";
+}
+
+include 'footerProd.html';
+
+>>>>>>> Stashed changes:ProyectoPHP/Indices/productos.php
 ?>
---
+++ ProyectoPHP/indices/carrito.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'headerProd.html';
+
+// Confirmar sesión
+session_start();
+
+if(!isset($_SESSION['loggedin'])) {
+    header('Location: productos.php');
+    exit();
+}
+
+echo "<h1 class=\"subtitulo\"> Carrito de compra </h1>";
+
+include 'footerProd.html';
+
+?>
