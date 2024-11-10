0fce52429a63b3e24cca69b5f1e7556aceeac32e
alfredo <alfredo.puentes25@outlook.com>
2024-11-10 00:03:02 -0600

    pagina de productos

--- ProyectoPHP/indices/productos.php
+++ ProyectoPHP/indices/productos.php
@@ -10,11 +10,39 @@ include 'headerProd.html';
 //     exit();
 // }
 
+// Conectar a la base de datos
+$conexion = mysqli_connect("localhost", "root", "", "tenis", 3306) or die("Problemas con la conexión");
+
 if (isset($_GET['producto'])) {
     $producto = $_GET['producto'];
     echo "<h1 class=\"subtitulo\"> Esta usted viendo el producto: ". $producto."</h1>";
+    $consulta = "SELECT * FROM tenis WHERE imagen = '".$producto."';";
+    $resultado = mysqli_query($conexion, $consulta);
+    $tenis = mysqli_fetch_assoc($resultado);
+    echo "<h1 class=\"fondo\">";
+    echo "Precio = ".$tenis["precio"]."<br>";
+    echo "Color = ".$tenis["color"]."<br>";
+    echo "Talla = ".$tenis["talla"]."<br>";
+    echo "Marca = ".$tenis["marca"]."<br>";
+    echo "Modelo = ".$tenis["modelo"]."<br>";
+    echo "<img src=\"../img/".$tenis["imagen"]."\" width=\"10%\">";
+    echo "<br></h1>";
+    mysqli_close($conexion);
 } else {
     echo "<h1 class=\"subtitulo\"> Esta usted viendo todos los productos </h1>";
+    $consulta = "SELECT * FROM tenis;";
+    $resultado = mysqli_query($conexion, $consulta);
+    while ($tenis = mysqli_fetch_assoc($resultado)) {
+        echo "<h1 class=\"fondo\">";
+        echo "Precio = ".$tenis["precio"]."<br>";
+        echo "Color = ".$tenis["color"]."<br>";
+        echo "Talla = ".$tenis["talla"]."<br>";
+        echo "Marca = ".$tenis["marca"]."<br>";
+        echo "Modelo = ".$tenis["modelo"]."<br>";
+        echo "<img src=\"../img/".$tenis["imagen"]."\" width=\"10%\">";
+        echo "<br></h1>";
+    }
+    mysqli_close($conexion);
 }
 
 include 'footerProd.html';
